3cd17b4bbb4401e01e1765f24e4c6f8b309266eb
jennyjohnston <jenny4johnston@gmail.com>
2019-05-20 20:43:03 -0400

    node-lab-upload

---
+++ quotes.js
@@ -0,0 +1,13 @@
+const quotes = ['live and let live', 
+                'everything happens for a reason', 
+                'this too shall pass', 
+                'you are enough; a thousand times enough',
+                'look for something positive in each day',
+                'creativity takes courage',
+                'difficult roads often lead to beautiful destinations',
+                'be fearless in the pursuit of what sets your soul on fire'
+            ];
+
+module.exports = {
+    quotes
+};
